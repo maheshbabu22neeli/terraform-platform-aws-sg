c8fc33f975764742514f8c3f5e570b28a3a380f9
Mahesh Babu Neeli <maheshbabu22.neeli@gmail.com>
2026-03-04 20:07:46 +1100

    Terraform SG: Not allowing single quotes in description

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_security_group" "main" {
 
   name        = "${var.project}-${var.environment}-${var.sg_name}" // roboshop-dev-mongodb  // roboshop-dev-catalogue
-  description = "Allow TLS inbound traffic for Project ${var.project} in '${var.environment}' for component '${var.sg_name}'"
+  description = "Allow TLS inbound traffic for Project ${var.project} in ${var.environment} for component ${var.sg_name}"
   vpc_id      = var.vpc_id
 
   // we can allow out going traffic from our instances
